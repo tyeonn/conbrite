b6cb13b6852210aaf8872f6f4380e739a2ea32e9
Timothy Wu <timothyjwu1@gmail.com>
2019-03-29 02:05:50 -0400

    session

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,17 +1,16 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER, RESET_SESSION_ERRORS} from '../actions/session_actions';
 
-const _nullError = [];
-const sessionErrorsReducer = (state = _nullError, action) => {
+const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_SESSION_ERRORS: 
-      return action.errors;
+      return action.errors || [];
     case RECEIVE_CURRENT_USER:
-      return state;
+      return [];
     case RESET_SESSION_ERRORS:
-      return state;
+      return [];
     default:
-      return state;
+      return [];
   }
 };
 
